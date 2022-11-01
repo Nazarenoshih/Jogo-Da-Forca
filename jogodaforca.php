1b32b61959ec26fb501a950ab63423b59a78045b
Nazareno Shih <110361082+Nazarenoshih@users.noreply.github.com>
2022-11-01 20:06:22 -0300

    melhoramento
    Está jogável de forma bem limitada.

--- jogodaforca.php
+++ jogodaforca.php
@@ -1,3 +1,8 @@
+<?php
+session_start();
+
+?>
+
 <!DOCTYPE html>
 <html>
 <head><title> Jogo da Forca </title>
@@ -5,33 +10,60 @@
 </head>
 
 <body>
-    <h4>Adivinhe o nome da fruta:</h4>
 	<form method="get">
-		<!-- <span>Digite seu nome:</span> -->
-		<!-- <input type="text" name="nome" placeholder="nome" required> -->
+	
+		<input type="submit" name="novo" value="Novo Jogo">
+	</form>
+
+	    <h4>Adivinhe o nome da fruta:</h4>
+	<form method="get">
+
 		<br>
 		<span>Digite uma letra:</span>
 		<input type="text" name="letra" placeholder="letra" required>
 		<br>
 	
-
 		<input type="submit" name="enviar" value="enviar">
 	</form>
 
+
 	<?php
-		if(isset($_GET["enviar"])){
-			$numeroTentativa=6;
-			$palavraCerta="Uva";
-			$palavraOculta="_ _ _";
-			$palavraOcultaTeste="___";
-			//$nome=$_GET["nome"];
-			$tentativaLetra=$_GET["letra"];
-
-			
-
-			echo "<br><h2>".$palavraOculta."</h2>";
-			echo "<br><h2>".$palavraOcultaTeste."</h2>";
+	if(isset($_GET["novo"])){
+		$_SESSION["tentativasTotais"]=6;
+		$_SESSION["tentativasUsadas"]=0;
+		$_SESSION["palavraCerta"]="Uva";
+		$_SESSION["palavraOculta"]=" _ _ _";
 		}
+
+		$palavraCerta=$_SESSION["palavraCerta"];
+		$palavraOculta=$_SESSION["palavraOculta"];
+
+
+	if(isset($_GET["enviar"])){
+	
+		$tentativaLetra=$_GET["letra"];
+
+		$indx=strpos($palavraCerta, $tentativaLetra);
+		echo $indx;
+
+		if($indx===false){
+			$_SESSION["tentativasUsadas"]=$_SESSION["tentativasUsadas"]+1;
+			echo "Errou.";
+		}
+		else{
+			$palavraOculta[$indx+1+1*$indx]=$tentativaLetra;
+			$_SESSION["palavraOculta"]=$palavraOculta;
+		}
+		//else{
+		//	$_SESSION["tentativasUsadas"]=$_SESSION["tentativasUsadas"]+1;
+		//}
+
+	}
+
+	echo "<br><h2>".$_SESSION["palavraOculta"]."</h2>";
+				
 	?>
+
+
 </body>
 </html>
